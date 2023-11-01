7cef143e36f6b6e49341c884a960029c37ffd929
M. Karthik Reddy <140091921+MARAM-KARTHIK-REDDY4@users.noreply.github.com>
2023-11-01 19:59:29 +0530

    Update installcompiler.sh

--- installcompiler.sh
+++ installcompiler.sh
@@ -8,8 +8,4 @@ pkg install cmatrix -y
 echo " "
 pkg install openjdk-17 
 echo " "
-/*
-after installation compiler 
-to run the java source file - command is javac filename.java
-to run the java class file - command is java filename
-*/
+
