148e2296079aa85a1c6cb894b30e1896a674e129
M. Karthik Reddy <140091921+MARAM-KARTHIK-REDDY4@users.noreply.github.com>
2023-11-01 19:55:07 +0530

    Update installcompiler.sh

--- installcompiler.sh
+++ installcompiler.sh
@@ -2,33 +2,14 @@ apt-get update -y
 echo " "
 apt-get upgrade -y
 echo " "
-pkg install figlet -y
-echo " "   
-pkg install toilet -y
-echo " "
-pkg install cowsay -y
-echo " "
-pkg install nano -y
-echo " "
-pkg install ruby -y
-echo " "
-gem install lolcat
-echo " "
-pkg install nano -y
-echo ""
-pkg install mpv -y
-echo " "
 pkg install git -y
-echo " "
-pkg install wget -y
-echo " "
-pkg install php -y
-echo " "
-pkg install python -y
-echo " "
-pkg install python2 -y
 echo " "
 pkg install cmatrix -y
 echo " "
-pkg install sl -y 
+pkg install openjdk-17 
 echo " "
+/*
+after installation compiler 
+to run the java source file - command is javac filename.java
+to run the java class file - command is java filename
+*/
